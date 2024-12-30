432ee59fdea12c3685d36eb34612917b54351ca0
hardik singh <realhardik18@gmail.com>
2024-12-30 11:37:04 +0530

    added in the images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,17 @@ export default function LandingPage() {
     { name: 'ZVIA Tech', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167768249499698/Group_1000002798.png?ex=67738783&is=67723603&hm=ceca71a0d6c3cd42a43eab5494cadc2fa20d72faaf61912dc84c6046c1bf14c2&=&format=webp&quality=lossless' },
     { name: 'MedX AI Innovations', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167768467472384/Group_1000002799.png?ex=67738783&is=67723603&hm=021414a7e3d863bba5aa4609672ca62504cde836e857c8f32343cd8876205df3&=&format=webp&quality=lossless' },
     { name: 'Stemtec AI & Robotics', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167791196672092/image_7.png?ex=67738788&is=67723608&hm=8896e4857785ee5c1ca1088cc6624db329366f9d66151fd2a200e107d72b7a7b&=&format=webp&quality=lossless' },
-    { name: 'CracKube', logo: 'https://images.unsplash.com/photo-1535378917042-10a22c95931a?w=800&auto=format&fit=crop&q=60' },
-    { name: 'Quinproc LLP', logo: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=800&auto=format&fit=crop&q=60' },
+    { name: 'CracKube', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167791401930845/image_8.png?ex=67738788&is=67723608&hm=20539753517b5107585f4aa6392067ab1fc9b147d85c96c3696d759f2b955cf8&=&format=webp&quality=lossless' },
+    { name: 'Quinproc LLP', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167791603253281/image_9.png?ex=67738788&is=67723608&hm=075d25fb4494a5b86c1069cfffc7a77e40e24fa8f7db4f160141703a328a4b1a&=&format=webp&quality=lossless' },
     { name: 'Mechonyx Automation', logo: 'https://images.unsplash.com/photo-1518770660439-4636190af475?w=800&auto=format&fit=crop&q=60' },
-    { name: 'Mafkin Robotics', logo: 'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=800&auto=format&fit=crop&q=60' },
-    { name: 'Chakaralaya Analytics', logo: 'https://images.unsplash.com/photo-1518770660439-4636190af475?w=800&auto=format&fit=crop&q=60' },
+    { name: 'Mafkin Robotics', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323169654151188590/image_12.png?ex=67738944&is=677237c4&hm=1a7a94a39c70819b4a3cfa164471b34cd1ba82dbb9794d48e6da75cf18ddcb54&=&format=webp&quality=lossless' },
+    { name: 'Chakaralaya Analytics', logo: 'https://media.discordapp.net/attachments/950058252899598386/1323167791838396467/image_10.png?ex=67738788&is=67723608&hm=343ae59570f32720e6d196dd1d26057417c3f5cef1330772a8e3963caeabe215&=&format=webp&quality=lossless' },
   ]
 
   return (
     <main className="min-h-screen relative overflow-hidden">
       {/* Animated gradient background */}
-      <div className="absolute inset-0 bg-gradient-to-r from-blue-600 via-purple-600 to-pink-500 animate-gradient bg-[length:200%_200%]" />
+      <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-400 animate-gradient bg-[length:200%_200%]" />
       
       {/* Content */}
       <div className="relative">
@@ -27,7 +27,7 @@ export default function LandingPage() {
         <section className="relative px-6 lg:px-8 py-12 md:py-24">
           <div className="absolute top-4 left-4">
             <Image
-              src="/placeholder.svg"
+              src="https://media.discordapp.net/attachments/950058252899598386/1323170217525907490/Hf4HeL3BvOv4W6cAAAAASUVORK5CYII.png?ex=677389cb&is=6772384b&hm=7c9be05070286af7e1c132104d9cfda9cc17bcc5b29e4cbceaf77eab82e42201&=&format=webp&quality=lossless"
               alt="VIT Logo"
               width={60}
               height={60}
@@ -76,7 +76,7 @@ export default function LandingPage() {
                 <Card className="p-8 space-y-4 bg-white/10 backdrop-blur-sm border-white/20">
                   <div className="flex justify-between items-center">
                     <Image
-                      src="/placeholder.svg"
+                      src="https://media.discordapp.net/attachments/950058252899598386/1323167768014749740/WhatsApp_Image_2024-12-11_at_19.22.56_b93d8e2d.jpg?ex=67738783&is=67723603&hm=22f37be638341bc8f99f3f2469034f5cdae6d4a0a7b24312a9bd12a393e0a27c&=&format=webp&width=716&height=395"
                       alt="V-NEST Logo"
                       width={120}
                       height={40}
